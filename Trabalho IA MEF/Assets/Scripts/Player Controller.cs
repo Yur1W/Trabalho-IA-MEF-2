11e0ec31391c6302b404e0c393ea7456f043b92f
Yur1W <165611276+Yur1W@users.noreply.github.com>
2025-10-01 15:45:51 -0300

    Tags/PlayerState

--- Trabalho IA MEF/Assets/Scripts/Player Controller.cs
+++ Trabalho IA MEF/Assets/Scripts/Player Controller.cs
@@ -1,20 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    //componentes
     protected Animator animator;
     protected Rigidbody2D rb;
     protected Collider2D coll;
     [Header("Player States")]
     [SerializeField]
     protected bool isRunning = false;
+    protected bool isAttacking = false;
+    protected bool isSliding = false;
+    protected bool isGrounded = true;
 
     [Header("Player Values")]
     [SerializeField]
     protected float playerSpeed = 5f;
+    [SerializeField]
+    protected float currentSpeed;
+    protected float JumpForce = 14f;
+    [SerializeField]
+    protected float slideSpeed = 8f;
+    [SerializeField]
+    protected float slideTime = 0.5f;
+
+    [SerializeField]
+    protected enum playerState { idle, running, jumping, falling, attacking, sliding, firing, hurt, throwing }
+    protected playerState state = playerState.idle; 
 
     void Start()
-    {
+    {   currentSpeed = playerSpeed;
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
@@ -23,31 +39,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Slide();
+       switch(state)
+       {
+            case playerState.idle:
+                Movement();
+                break;
+       }
     }
     void Slide()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift) && isRunning)
         {
             animator.SetTrigger("slided");
+            currentSpeed = slideSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            currentSpeed = playerSpeed;
         }
 
     }
 
     void Movement()
-    {
+    {   
+        
         float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * playerSpeed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(moveInput * currentSpeed, rb.linearVelocityY);
 
         if (moveInput != 0)
         {
             isRunning = true;
             animator.SetBool("isRunning", true);
+            state = playerState.running;
         }
         else
         {
             isRunning = false;
             animator.SetBool("isRunning", false);
+        }
+    }
+    void slash()
+    {
+        isAttacking = Input.GetKeyDown(KeyCode.P);
+        if (isAttacking && !isSliding && !isRunning)
+        {
+            isAttacking = true;
+            animator.SetTrigger("slashed");
+            state = playerState.attacking;
         }
     }
 }
